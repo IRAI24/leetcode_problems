8c35f42bfb841d5ab90f24e3b96e949782abd03a
IRAIANBU S <iraianbu24@gmail.com>
2025-08-20 22:03:48 +0530

    Time: 0 ms (100%), Space: 40.5 MB (51.02%) - LeetHub

---
+++ 0233-number-of-digit-one/0233-number-of-digit-one.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countDigitOne(int n) {
+        int  base=1;
+        int sum=0;
+        while(base<=n)
+        {
+            int lt=n/base/10;
+            int cur=(n/base)%10;
+            int rt=n%base;
+            if(cur<1)sum+=lt*base;
+            else if(cur>1)sum+=(lt+1)*base;
+            else if(cur==1)sum+=lt*base+rt+1;
+            base*=10;
+        }
+        return sum;
+    }
+}
